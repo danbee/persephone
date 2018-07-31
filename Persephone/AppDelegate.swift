6c38b7435e0befb19210a0467ad9ec4524a4a461
Dan Barber <github@danbarber.me>
2018-07-31 13:05:54 -0400

    Initial Commit

---
+++ Persephone/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Persephone
+//
+//  Created by Daniel Barber on 2018/7/31.
+//  Copyright © 2018 Dan Barber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Persephone/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Persephone
+//
+//  Created by Daniel Barber on 2018/7/31.
+//  Copyright © 2018 Dan Barber. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
