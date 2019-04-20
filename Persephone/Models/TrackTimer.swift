d076ef311b4bb34d9ce50c80472dc78a681f689e
Daniel Barber <github@danbarber.me>
2019-04-19 20:17:45 -0500

    We need to run the timer on the main thread
    This adds it to the main RunLoop.

--- Persephone/Models/TrackTimer.swift
+++ Persephone/Models/TrackTimer.swift
@@ -17,22 +17,23 @@ class TrackTimer: NSObject {
     print("Starting timer")
     guard let elapsedTimeMs = elapsedTimeMs else { return }
     print(elapsedTimeMs)
-    
+
     timer?.invalidate()
 
     startTime = CACurrentMediaTime()
     startElapsed = Double(elapsedTimeMs) / 1000
 
-    timer = Timer.scheduledTimer(
-      withTimeInterval: 0.25,
-      repeats: true
-    ) { _ in
-      let currentTime = CACurrentMediaTime()
+    DispatchQueue.main.sync {
+      self.timer = Timer.scheduledTimer(
+        withTimeInterval: 0.25,
+        repeats: true
+      ) { _ in
+        print("Timer fired")
+        let currentTime = CACurrentMediaTime()
 
-      let timeDiff = currentTime - self.startTime
-      let newElapsedTimeMs = UInt((self.startElapsed + timeDiff) * 1000)
+        let timeDiff = currentTime - self.startTime
+        let newElapsedTimeMs = UInt((self.startElapsed + timeDiff) * 1000)
 
-      DispatchQueue.main.async {
         AppDelegate.store.dispatch(
           UpdateElapsedTimeAction(elapsedTimeMs: newElapsedTimeMs)
         )
