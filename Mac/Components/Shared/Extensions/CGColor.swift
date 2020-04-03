9228c68cf9c90ca84821a4116742e5620209b6a1
Daniel Barber <github@danbarber.me>
2020-04-03 12:05:26 -0400

    Split CGColor extension
    There doesn't seem to be any overlap in the available methods for
    specifying a color on iOS or macOS.

---
+++ Mac/Components/Shared/Extensions/CGColor.swift
@@ -0,0 +1,14 @@
+//
+//  NSColor.swift
+//  Persephone
+//
+//  Created by Daniel Barber on 2019/2/16.
+//  Copyright © 2019 Dan Barber. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGColor {
+  static let albumBorderColorLight = CGColor.black.copy(alpha: 0.15)
+  static let albumBorderColorDark = CGColor.white.copy(alpha: 0.15)
+}
